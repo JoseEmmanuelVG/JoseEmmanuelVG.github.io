cb5ff48452d8f4e658fa283e09e0ac6208b4f756
José Emmanuel Vázquez Galán <89156254+JoseEmmanuelVG@users.noreply.github.com>
2025-08-26 23:53:36 -0600

    Update next.config.ts

--- cv-next/next.config.ts
+++ cv-next/next.config.ts
@@ -1,9 +1,15 @@
 // next.config.ts
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+});
 
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
+  pageExtensions: ["ts", "tsx", "mdx"],
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
